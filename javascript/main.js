16eccaa27a5a75ca08160875ad4dfdd7095f53b3
surajsoankamble <sk.suraj57@gmail.com>
2020-12-26 14:26:44 +0530

    wiring textarea input

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,14 @@
+//button reference
 var btn=document.querySelector("#btn");
+
+//text area reference
+var txtInput=document.querySelector("#txt-input");
+
+//button clicking event
 btn.addEventListener("click",clickEventHandler);
+
 //function for click
 function clickEventHandler(){
     console.log("clicked");
+    console.log("this is your text "+txtInput.value)
 }
